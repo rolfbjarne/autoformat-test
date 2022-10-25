6508782b8f312892a6ba02aee8a377d4a733747b
rolfkvinge <116664754+rolfkvinge@users.noreply.github.com>
2022-10-25 18:56:02 +0200

    Update Program.cs

--- testproject/Program.cs
+++ testproject/Program.cs
@@ -1,4 +1,4 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine ("Hello, World!");
-Console.WriteLine ("Hello, Abomination!");
+Console . WriteLine   ("Hello, Abomination!") ;
 
