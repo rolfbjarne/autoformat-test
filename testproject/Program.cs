03204c26d98b5cd2cda31fe67ee0b934067e0802
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-10-25 19:02:58 +0200

    Update Program.cs

--- testproject/Program.cs
+++ testproject/Program.cs
@@ -1,6 +1,3 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine ("Hello, World!");
-Console.WriteLine ("Hello, Abomination!");
-
-
-
+Console . WriteLine ("Hello, Abomination!");
